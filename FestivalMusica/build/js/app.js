6f911c4378782f2ac6dde8ab58629df1c7aba663
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-08-01 16:12:54 -0500

    feat: :sparkles: Add new functions to the galeria and to the header

--- FestivalMusica/build/js/app.js
+++ FestivalMusica/build/js/app.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    crearGaleria();
 
+    navegacionFija();
+    crearGaleria();
+    resaltarEnlace();
+    scrollNav();
 });
+
+function navegacionFija() {
+    const header = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+    
+    document.addEventListener('scroll', () => {
+        if(sobreFestival.getBoundingClientRect().bottom < 1){
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    })
+}
 
 function crearGaleria (){
     const CANTIDAD_IMAGENES = 16;
@@ -22,16 +38,86 @@ function crearGaleria (){
 }
 
 function mostrarImagen(i) {
+    const imagen = document.createElement('IMG');
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = 'imagen Galeria';
+    
     //Generar el modal
     const modal = document.createElement('DIV');
     modal.classList.add('modal');
     modal.onclick = cerrarModal;
+    
+    //Boton cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON');
+    cerrarModalBtn.textContent = 'X';
+    cerrarModalBtn.classList.add('btn-cerrar');
+    cerrarModalBtn.onclick = cerrarModal;
+    
+    modal.appendChild(imagen);
+    modal.appendChild(cerrarModalBtn);
+
     //Agregar al HTML
     const body = document.querySelector('body');
+    body.classList.add('overflow-hidden');
     body.appendChild(modal);
 }
 
 function cerrarModal() {
     const modal = document.querySelector('.modal');
-    modal?.remove();
+    modal.classList.add('fade-out');
+
+    setTimeout(() => {
+        modal?.remove();
+        const body = document.querySelector('body');
+        body.classList.remove('overflow-hidden');
+    }, 500);
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', () => {
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('.navegacion-principal a');
+        let actual = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop; // obtiene la distancia desde la parte superior de la página hasta el borde superior de la sección actual.
+            const sectionHeight = section.clientHeight; // obtiene la altura de la sección actual.
+
+            /*
+                Verifica si la posición actual de desplazamiento de la ventana es mayor o igual 
+                a la distancia desde la parte superior de la página hasta el borde superior de 
+                la sección actual menos un tercio de la altura de la sección actual. Si es así, 
+                establece el ID de la sección actual en la variable actual
+            */ 
+            if(window.scrollY >= sectionTop - sectionHeight / 3){
+                actual = section.id;
+            }
+        })
+
+        /*
+            Recorre cada enlace de navegación y elimina la clase active si la tiene. 
+            Luego, verifica si el atributo href del enlace es igual al ID de la sección actual. 
+            Si es así, agrega la clase active al enlace.
+        */
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === `#${actual}`){
+                link.classList.add('active');
+            } 
+        })
+    })
+}
+
+function scrollNav() {
+    const navLinks = document.querySelectorAll('.navegacion-principal a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            const sectionScroll = e.target.getAttribute('href');
+            const section = document.querySelector(sectionScroll);
+
+            section.scrollIntoView({behavior: 'smooth'});
+        })
+    })
 }
