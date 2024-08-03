d9b52456df434a567d7d2d1e034f96a2c57ff85f
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-08-03 12:19:20 -0500

    feat: :sparkles: Learning new js features

---
+++ IntroduccionJS-FINALIZADO/js/43.js
@@ -0,0 +1,32 @@
+// Eventos del DOM - Submit
+const formulario = document.querySelector('#formulario');
+formulario.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const nombre = document.querySelector('#nombre').value;
+    const password = document.querySelector('#password').value;
+
+    // Prevenir nuevas alertas 
+    const alertaPrevia = document.querySelector('.alerta');
+    if (alertaPrevia) {
+        alertaPrevia.remove();
+    }
+
+    const alerta = document.createElement('DIV');
+
+    alerta.classList.add('alerta', 'text-white', 'uppercase', 'text-sm', 'text-center', 'p-2', 'font-black');
+    
+    
+    if (nombre === '' || password === '') {
+        alerta.textContent = 'Todos los campos son obligatorios';
+        alerta.classList.add('bg-red-500');
+    } else {
+        alerta.textContent = 'Todo bien...';
+        alerta.classList.add('bg-green-500');
+    }
+
+    formulario.appendChild(alerta);
+    setTimeout(() => {
+        alerta.remove();
+    }, 5000);
+})
