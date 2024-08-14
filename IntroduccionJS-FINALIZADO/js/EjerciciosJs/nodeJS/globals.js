fd3ccd7ceb225f2ed7ecb0fa6b232967a1ef0911
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-08-14 05:57:29 -0500

    feat: :sparkles: Add new files for learning and practice js in the server with  nodeJS

---
+++ IntroduccionJS-FINALIZADO/js/EjerciciosJs/nodeJS/globals.js
@@ -0,0 +1,5 @@
+console.log(__dirname);  // Donde esta el archivo global.js
+console.log(__filename); // Me da la rura completa de global.js
+console.log(module); // Me da la informacion del modulo
+console.log(require); 
+console.log(process); 
