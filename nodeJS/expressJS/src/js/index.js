3288b6d24091093c0c771de16adfdf04d2ef3e6b
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-08-26 21:39:00 -0500

    feat: :sparkles: Add new features to learn how make views with the ejs module

---
+++ nodeJS/expressJS/src/js/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+require('ejs');
+
+const app = express();
+
+const homeRoutes = require('../routes/home');
+const userRoutes = require('../routes/users');
+
+// Settings 
+app.set('case sensitive routing', true);
+app.set('appName', 'Express Tutorial');
+app.set('port', 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
+// Middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use(homeRoutes);
+app.use(userRoutes);
+
+// Routes
+app.use('public', express.static(path.join(__dirname, '../public')));
+app.use("uploads", express.static(path.join(__dirname, '../uploads')));
+
+app.listen(app.get('port'));
+console.log(`Server ${app.get('appName')} on port ${app.get('port')}`);
