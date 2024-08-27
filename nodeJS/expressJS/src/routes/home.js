df9abfb223266dfdd7c529628619baf259f9e77e
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-08-26 21:38:06 -0500

    feat: :sparkles: Add a new JS file to learn how to create default routes

---
+++ nodeJS/expressJS/src/routes/home.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        tittle: 'Index page'
+    });
+});
+
+router.get('/about', (req, res) => {
+
+    res.render('about');
+})
+
+router.get('/dashboard', (req,res) => {
+    res.render('dashboard');
+})
+
+module.exports = router;
