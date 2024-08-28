be999192708385c07870572b858c3c6e2526b42c
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-08-28 05:24:14 -0500

    feat: :sparkles: Add simulation to connect to a Data Base

--- nodeJS/expressJS/src/index.js
+++ nodeJS/expressJS/src/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const connectDB = require('./db')
 require('ejs');
+
+connectDB()
 
 const app = express();
 
-const homeRoutes = require('../routes/home');
-const userRoutes = require('../routes/users');
+const homeRoutes = require('./routes/home');
+const userRoutes = require('./routes/users');
 
 // Settings 
 app.set('case sensitive routing', true);
 app.set('appName', 'Express Tutorial');
 app.set('port', 3000);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, './views'));
 
 // Middlewares
 app.use(express.json());
@@ -23,8 +26,8 @@ app.use(homeRoutes);
 app.use(userRoutes);
 
 // Routes
-app.use('public', express.static(path.join(__dirname, '../public')));
-app.use("uploads", express.static(path.join(__dirname, '../uploads')));
+app.use('public', express.static(path.join(__dirname, './public')));
+app.use("uploads", express.static(path.join(__dirname, './uploads')));
 
 app.listen(app.get('port'));
 console.log(`Server ${app.get('appName')} on port ${app.get('port')}`);
