0dd1b0c7e770da703d9395d8634bca1eff91a53b
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-08-28 05:26:55 -0500

    feat: :sparkles: Add route post to consume the API swapi of starwars

--- nodeJS/expressJS/src/routes/home.js
+++ nodeJS/expressJS/src/routes/home.js
@@ -1,4 +1,6 @@
+const { render } = require('ejs');
 const { Router } = require('express');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const router = Router();
 
@@ -16,5 +18,22 @@ router.get('/about', (req, res) => {
 router.get('/dashboard', (req,res) => {
     res.render('dashboard');
 })
+
+router.get('/posts', async (req, res) => {
+    try {
+        const response = await fetch('https://swapi.dev/api/people/');
+        const data = await response.json();
+        const results = data.results
+        console.log(results)
+        
+        res.render('posts', {
+            people: results,
+        });
+
+    } catch (error) {
+        console.error('Error:', error)
+        res.status(500).send('Error en la solicitud');
+    }
+})  
 
 module.exports = router;
